13f72c25a7d85d6dc15c247c4d76c5884a91d131
GlenIsIsaw <glenpabico18@gmail.com>
2024-04-11 09:12:51 +0800

    Added: Enable Page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [isUnderMaintenance, setIsUnderMaintenance] = useState(true);
+  const [isUnderMaintenance, setIsUnderMaintenance] = useState(false);
   return (
 
     <>
