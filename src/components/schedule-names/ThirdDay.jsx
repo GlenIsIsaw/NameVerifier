bf34929db4b6151b77e3144d92713f8763c54e04
glen <glenpabico18@gmail.com>
2025-03-09 23:30:35 +0800

    Fixing bugs

--- src/components/schedule-names/ThirdDay.jsx
+++ src/components/schedule-names/ThirdDay.jsx
@@ -5,7 +5,7 @@ import PopCard from "../PopCard";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import InputGroup from "react-bootstrap/InputGroup";
-import thirdDay from "../../Data/thirdDay"; // Import the actual data
+import { thirdDay } from "../../Data/thirdDay"; // Import the actual data
 import '../../App.css';
 
 const ThirdDay = () => {
